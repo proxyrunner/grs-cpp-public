2482ede8d87dc0c130e9f3162aa534de72d4f82a
gil-ryan <gil.s4@outlook.com>
2019-11-05 22:19:21 -0500

    COMMIT
    COMMIT

---
+++ strings.cpp
@@ -0,0 +1,10 @@
+// Simple file to play around with strings.
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void){
+string s = "test string";
+cout << "\n" <<  s << endl;
+}
